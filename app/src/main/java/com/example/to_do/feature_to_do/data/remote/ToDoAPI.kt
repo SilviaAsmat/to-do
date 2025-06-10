009ddc6c7dd60b725cb8fdc873c6be30b2afa132
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-10 16:31:38 -0500

    Created suspend functions interact with api

--- app/src/main/java/com/example/to_do/feature_to_do/data/remote/ToDoAPI.kt
+++ app/src/main/java/com/example/to_do/feature_to_do/data/remote/ToDoAPI.kt
@@ -1,4 +1,32 @@
-package com.example.to_do.feature_to_do.data.remote.dto
+package com.example.to_do.feature_to_do.data.remote
+
+import com.example.to_do.feature_to_do.data.remote.dto.RemoteToDoItem
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Url
 
 interface ToDoAPI {
+    @GET("todo.json")
+    suspend fun getAllToDos(): List<RemoteToDoItem>
+
+    @GET("todo.json?orderBy=\"ID\"")
+    suspend fun getSingleToDoById(@retrofit2.http.Query("equalTo")id: Int?): Map<String, RemoteToDoItem>
+
+//    @POST("todo.json")
+//    sus pend fun addToDo(@Body url:String, @Body updatedToDo: RemoteToDoItem): Response<Unit>
+
+    @PUT
+    suspend fun addToDo(@Url url: String, @Body updatedToDo: RemoteToDoItem): Response<Unit>
+
+    @DELETE("todo/{id}.json")
+    suspend fun deleteToDo(@Path("id") id :Int?) : Response<Unit>
+
+    @PUT("todo/{id}.json")
+    suspend fun updateToDoItem(@Path("id") id: Int?, @Body toDoItem: RemoteToDoItem) : Response<Unit>
+
 }
