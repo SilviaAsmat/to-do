797f4d7f3e703035966d11c2bb5e6b068fedaace
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-10 16:32:36 -0500

    Created mapper to allow interaction between models

--- app/src/main/java/com/example/to_do/feature_to_do/data/mapper/ToDoMapper.kt
+++ app/src/main/java/com/example/to_do/feature_to_do/data/mapper/ToDoMapper.kt
@@ -1,4 +1,151 @@
 package com.example.to_do.feature_to_do.data.mapper
 
-class ToDoMapper {
+import com.example.to_do.feature_to_do.data.local.dto.LocalToDoItem
+import com.example.to_do.feature_to_do.data.remote.dto.RemoteToDoItem
+import com.example.to_do.feature_to_do.domain.model.ToDoItem
+
+fun ToDoItem.toLocalToDoItem(): LocalToDoItem {
+    return LocalToDoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
 }
+
+fun ToDoItem.toRemoteToDoItem(): RemoteToDoItem {
+    return RemoteToDoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun LocalToDoItem.toToDoItem(): ToDoItem{
+    return ToDoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun LocalToDoItem.toRemoteToDoItem(): RemoteToDoItem{
+    return RemoteToDoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun RemoteToDoItem.toToDoItem(): ToDoItem{
+    return ToDoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun RemoteToDoItem.toLocalToDoItem(): LocalToDoItem{
+    return LocalToDoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun List<ToDoItem>.toLocalToDoItemList(): List<LocalToDoItem>{
+    return this.map {toDo->
+        LocalToDoItem(
+            title = toDo.title,
+            description = toDo.description,
+            timestamp = toDo.timestamp,
+            completed = toDo.completed,
+            archived = toDo.archived,
+            id = toDo.id
+        )
+    }
+}
+
+fun List<ToDoItem>.toRemoteToDoItemList(): List<RemoteToDoItem>{
+    return this.map {toDo->
+        RemoteToDoItem(
+            title = toDo.title,
+            description = toDo.description,
+            timestamp = toDo.timestamp,
+            completed = toDo.completed,
+            archived = toDo.archived,
+            id = toDo.id
+        )
+    }
+}
+
+fun List<LocalToDoItem>.toToDoItemListFromLocal(): List<ToDoItem>{
+    return this.map {toDo->
+        ToDoItem(
+            title = toDo.title,
+            description = toDo.description,
+            timestamp = toDo.timestamp,
+            completed = toDo.completed,
+            archived = toDo.archived,
+            id = toDo.id
+        )
+    }
+}
+
+fun List<LocalToDoItem>.toRemoteToDoItemListFromLocal(): List<RemoteToDoItem>{
+    return this.map {toDo->
+        RemoteToDoItem(
+            title = toDo.title,
+            description = toDo.description,
+            timestamp = toDo.timestamp,
+            completed = toDo.completed,
+            archived = toDo.archived,
+            id = toDo.id
+        )
+    }
+}
+
+fun List<RemoteToDoItem>.toToDoItemListFromRemote(): List<ToDoItem>{
+    return this.map {toDo->
+        ToDoItem(
+            title = toDo.title,
+            description = toDo.description,
+            timestamp = toDo.timestamp,
+            completed = toDo.completed,
+            archived = toDo.archived,
+            id = toDo.id
+        )
+    }
+}
+
+fun List<RemoteToDoItem>.toLocalToDoItemListFromRemote(): List<LocalToDoItem>{
+    return this.map {toDo->
+        LocalToDoItem(
+            title = toDo.title,
+            description = toDo.description,
+            timestamp = toDo.timestamp,
+            completed = toDo.completed,
+            archived = toDo.archived,
+            id = toDo.id
+        )
+    }
+}
+
+
