6e59d9e126f03338940409646d89f3e1571440ac
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-11 11:49:46 -0500

    Created interface for the domain layer to access data in a separated way

--- app/src/main/java/com/example/to_do/feature_to_do/domain/repo/ToDoListRepo.kt
+++ app/src/main/java/com/example/to_do/feature_to_do/domain/repo/ToDoListRepo.kt
@@ -1,4 +1,13 @@
-package com.example.to_do.feature_to_do.data.repo
+package com.example.to_do.feature_to_do.domain.repo
 
-class ToDoListRepo {
+import com.example.to_do.feature_to_do.domain.model.ToDoItem
+
+interface ToDoListRepo {
+    suspend fun getAllToDos(): List<ToDoItem>
+    suspend fun getAllToDosFromLocalCache(): List<ToDoItem>
+    suspend fun getAllToDosFromRemote()
+    suspend fun getSingleToDoItemById(id: Int): ToDoItem?
+    suspend fun addToDoItem(todo: ToDoItem)
+    suspend fun updateToDoItem(todo: ToDoItem)
+    suspend fun deleteToDoItem(todo: ToDoItem)
 }
