73e27ba29ecaf4118f4c3e8b4e7069cac48b5ed2
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-11 11:48:42 -0500

    Created to allow for coroutine dispatcher use along with Hilt

--- app/src/main/java/com/example/to_do/feature_to_do/data/di/DispatcherModule.kt
+++ app/src/main/java/com/example/to_do/feature_to_do/data/di/DispatcherModule.kt
@@ -1,4 +1,17 @@
 package com.example.to_do.feature_to_do.data.di
 
+import dagger.Provides
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
 object DispatcherModule {
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 }
