4024a604d8e563f667e1b7c49ddce92dd570d459
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-12 13:46:38 -0500

    Added error message

--- app/src/main/java/com/example/to_do/core/util/ToDoConstants.kt
+++ app/src/main/java/com/example/to_do/core/util/ToDoConstants.kt
@@ -3,3 +3,7 @@ package com.example.to_do.core.util
 object ToDoUseCaseStrings {
     const val EMPTY_TITLE_OR_DESCRIPTION = "Both the ToDo title and description must be populated"
 }
+
+object ToDoListStrings {
+    const val CANT_GET_TODOS = "Error: Could not retrieve ToDo Items"
+}
