928582fc4ef9c8eb69142054f524619dec2fc3fc
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-12 12:42:36 -0500

    Created class for presentation layer to show state of todo item

--- app/src/main/java/com/example/to_do/feature_to_do/presentation/to_do_list/components/ToDoListState.kt
+++ app/src/main/java/com/example/to_do/feature_to_do/presentation/to_do_list/components/ToDoListState.kt
@@ -1,3 +1,12 @@
 package com.example.to_do.feature_to_do.presentation.to_do_list.components
 
-data class ToDoListState()
+import com.example.to_do.feature_to_do.domain.model.ToDoItem
+import com.example.to_do.feature_to_do.domain.util.SortingDirection
+import com.example.to_do.feature_to_do.domain.util.ToDoItemOrder
+
+data class ToDoListState(
+    val todoItems: List<ToDoItem> = emptyList(),
+    val todoItemOrder: ToDoItemOrder = ToDoItemOrder.Time(SortingDirection.Down, true),
+    val isLoading: Boolean = true,
+    val error: String? = null
+    )
