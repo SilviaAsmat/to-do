fce00cfdc5cf34692e4a5b9e3cb8776757233f46
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-12 12:43:10 -0500

    Created view model using Hilt

--- app/src/main/java/com/example/to_do/feature_to_do/presentation/to_do_list/components/ToDoListViewModel.kt
+++ app/src/main/java/com/example/to_do/feature_to_do/presentation/to_do_list/components/ToDoListViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.to_do.feature_to_do.presentation.to_do_list.components
 
-class ToDoListViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.to_do.feature_to_do.domain.use_case.ToDoUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+@HiltViewModel
+class ToDoListViewModel@Inject constructor(
+    private val toDoUseCases: ToDoUseCases,
+    private val dispatcher: CoroutineDispatcher
+) : ViewModel() {
 }
