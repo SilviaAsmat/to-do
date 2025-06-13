89d83a806554301e85bd0f86f0e37336fb46483c
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-13 13:19:12 -0500

    Added colors for icon status

--- app/src/main/java/com/example/to_do/core/presentation/components/getToDoColors.kt
+++ app/src/main/java/com/example/to_do/core/presentation/components/getToDoColors.kt
@@ -1,2 +1,34 @@
 package com.example.to_do.core.presentation.components
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.example.to_do.feature_to_do.domain.model.ToDoItem
+
+data class ToDoItemColors(
+    val backgroundColor: Color,
+    val textColor: Color,
+    val archiveIconColor: Color,
+    val checkColor: Color
+)
+
+@Composable
+fun getToDoColors(todo: ToDoItem): ToDoItemColors{
+    return if(todo.archived){
+        ToDoItemColors(
+            backgroundColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.6f),
+            textColor = MaterialTheme.colorScheme.onSecondary,
+            archiveIconColor = MaterialTheme.colorScheme.onSecondary,
+            checkColor = if(todo.completed) MaterialTheme.colorScheme.tertiaryContainer
+                else MaterialTheme.colorScheme.onSecondary
+        )
+    } else {
+        ToDoItemColors(
+            backgroundColor = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.6f),
+            textColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            archiveIconColor = MaterialTheme.colorScheme.secondary,
+            checkColor = if(todo.completed) MaterialTheme.colorScheme.tertiaryContainer
+            else MaterialTheme.colorScheme.secondary
+        )
+    }
+}
