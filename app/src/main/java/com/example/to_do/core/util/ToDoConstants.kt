89d52ce89f95dffd16727dfcc00b2a30787c03af
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-16 10:47:39 -0500

    Added string constants

--- app/src/main/java/com/example/to_do/core/util/ToDoConstants.kt
+++ app/src/main/java/com/example/to_do/core/util/ToDoConstants.kt
@@ -5,11 +5,23 @@ object ToDoUseCaseStrings {
 }
 
 object ToDoListStrings {
-    const val CANT_GET_TODOS = "Error: Could not retrieve ToDo Items"
+    const val CANT_GET_TODOS = "Error: Could not retrieve Todo Items"
+    const val TODO_LIST = "TODO List"
+    const val TITLE = "Title"
+    const val TIME = "Time"
+    const val Completed = "Completed"
+    const val SORT_UP = "Sort Up"
+    const val SORT_DOWN = "Sort Down"
+    const val SHOW_ARCHIVED = "Show Archived"
+    const val SORT_BY = "Sort By"
+    const val TODO_ITEM_DELETED = "Todo Item has been deleted"
+    const val UNDO = "Undo"
+
 }
 
 object ContentDescriptions {
     const val DELETE_TODO_ITEM = "Delete todo item"
     const val COMPLETE_TODO_ITEM = "Complete todo item"
     const val ARCHIVE_TODO_ITEM = "Archive todo item"
+    const val SELECTED = "Selected"
 }
